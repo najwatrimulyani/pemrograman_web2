205f95f1f3da43f19f51744407d44b1d28ba1135
najwatrimulyani <najwatri325@gmail.com>
2026-09-13 21:47:46 +0700

    Add files via upload

---
+++ Latihan_pertemuan2/latihan2.php
@@ -0,0 +1,14 @@
+<?php
+//inisiasi variabel yang digunakan
+
+//nama peralatan
+$brg1 = "Buku";
+$brg2 = "Mouse";
+$brg3 = "FlashDisk";
+$brg4 = "Pulpen";
+
+//harga perunit peralatan
+$harga1 = 17500;
+$harga2 = 30000;
+$harga3 = 70000;
+$harga4 = 22300;
