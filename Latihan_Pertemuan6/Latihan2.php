826c895be7320b82b579b86affc17ba330689424
najwatrimulyani <najwatri325@gmail.com>
2026-09-24 16:08:39 +0700

    Add files via upload

---
+++ Latihan_Pertemuan6/Latihan2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Latihan Array PHP</title>
+</head>
+<body>
+
+<?php
+
+$x = array("one", "two", "three");
+
+foreach ($x as $value) {
+    echo $value . "<br>";
+}
+
+?>
+
+</body>
+</html>
